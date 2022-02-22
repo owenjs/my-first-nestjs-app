2810f0d12f6110de2dc7534827305f513f1d20b9
Owen Evans <owen@3sidedcube.com>
2022-02-22 15:34:42 +0000

    env variables using config package

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,18 @@
 import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
+import * as config from "config";
 
 let ormconfig: MysqlConnectionOptions = {
   type: "mysql",
-  host: "db",
-  port: 3306,
-  username: "root",
-  password: "password",
-  database: "db",
+  host: config.get("db.host"),
+  port: parseInt(config.get("db.port"), 10),
+  username: config.get("db.username"),
+  password: config.get("db.password"),
+  database: config.get("db.database"),
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: true
 };
 
-if (process.env.NODE_ENV === "production") {
+if (config.get("env") === "production") {
   ormconfig = {
     ...ormconfig,
     synchronize: false,
